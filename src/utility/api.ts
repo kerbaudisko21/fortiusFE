875ef422f1328e3a964caf12cfd01077a31322bc
kerbaudisko21 <wilbertw99@gmail.com>
2024-02-12 03:31:46 +0700

    push

--- src/utility/api.ts
+++ src/utility/api.ts
@@ -1,6 +1,8 @@
 const getApi = () => {
     if (window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1') {
         return 'http://localhost:8000';
+    } else {
+        return 'https://fortius-api.vercel.app/api/api/v1'
     }
 };
 
